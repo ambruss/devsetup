ecd31cff48d199c8c89df1e4c6f577cd808cc736
ambrus <simon.ambrus@gmail.com>
2020-07-21 23:17:58 +0200

    Fix gcloud install

--- modules/google-cloud-sdk.sh
+++ modules/google-cloud-sdk.sh
@@ -5,9 +5,11 @@ is_installed() {
 }
 
 install() {
-    GCLOUD_VER=$(latest https://cloud.google.com/sdk/docs/release-notes ">$VERSION_RE \(")
-    GCLOUD_URL=https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-$GCLOUD_VER-linux-x86_64.tar.gz
-    curl "$GCLOUD_URL" | tar xvz google-cloud-sdk
-    # TODO noninteractive
-    ./google-cloud-sdk/install.sh
+    VER=$(latest https://cloud.google.com/sdk/docs/release-notes ">$VERSION_RE \(")
+    URL=https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-$VER-linux-x86_64.tar.gz
+    cd "$LOCAL" || fail "Couldn't cd into $LOCAL"
+    curl "$URL" | tar xvz google-cloud-sdk
+    ./google-cloud-sdk/install.sh --quiet --usage-reporting false
+    PATHRC="${LOCAL/$HOME/\~}/google-cloud-sdk/path.zsh.inc"
+    grep -q "$PATHRC" ~/.zshrc || echo "[ -f $PATHRC ] && source $PATHRC" >>~/.zshrc
 }
