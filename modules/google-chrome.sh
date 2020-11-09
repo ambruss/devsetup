692400dc17a811da8c53a0b748222be09108163a
ambrus <ambrussimon@flywheel.io>
2020-11-09 17:14:28 +0100

    Add recommended chrome extensions (open in tabs)

--- modules/google-chrome.sh
+++ modules/google-chrome.sh
@@ -7,4 +7,18 @@ is_installed() {
 install() {
     curl -O https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
     sudo dpkg --install google-chrome-stable_current_amd64.deb || sudo apt-get install -fqqy
+    for EXTENSION in "${EXTENSIONS[@]}"; do
+        google-chrome "$EXTENSION"
+    done
 }
+
+EXTENSIONS=(
+https://chrome.google.com/webstore/detail/adblock-plus-free-ad-bloc/cfhdojbkjhnklbpkdaibdccddilifddb
+https://chrome.google.com/webstore/detail/i-dont-care-about-cookies/fihnjjcciajhdojfnbdddfaoknhalnja
+https://chrome.google.com/webstore/detail/link-to-text-fragment/pbcodcjpfjdpcineamnnmbkkmkdpajjg
+https://chrome.google.com/webstore/detail/markdown-here/elifhakcjgalahccnjkneoccemfahfoa
+https://chrome.google.com/webstore/detail/markdown-to-jira/jjmmhbaklhogjgcccbmhfllhmfdamagg
+https://chrome.google.com/webstore/detail/markdown-viewer/ckkdlimhmcjmikdlpkmbgfkaikojcbjk
+https://chrome.google.com/webstore/detail/quick-jira/acdnmaeifljongleeegkkfnfcopblokj
+https://chrome.google.com/webstore/detail/talend-api-tester-free-ed/aejoelaoggembcahagimdiliamlcdmfm
+)
