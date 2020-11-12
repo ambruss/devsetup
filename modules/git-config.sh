b96306611b030fc3497167618d7a1cf4f2c9f9d2
Ambrus Simon <simon.ambrus@gmail.com>
2020-11-12 10:46:18 +0100

    Add push.default current and simplify git config

--- modules/git-config.sh
+++ modules/git-config.sh
@@ -5,31 +5,31 @@ is_installed() {
 }
 
 install() {
-    gitconf help.autocorrect 1
-
     gitconf alias.lg    "log --abbrev-commit --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
     gitconf alias.diff  "diff --no-color --no-pager"
     gitconf alias.undo  "reset HEAD~1 --mixed"
     gitconf alias.alias "config --get-regexp ^alias\. | sed -E 's/^alias.//;s/ /\t= /'"
-
     gitconf core.pager  "diff-so-fancy | less --tabs=4 -RFX"
-    gitconf diff-so-fancy.rulerWidth 47
-    gitconf --bool diff-so-fancy.markEmptyLines false
-    gitconf --bool diff-so-fancy.changeHunkIndicators false
-    gitconf --bool diff-so-fancy.stripLeadingSymbols false
+    gitconf help.autocorrect 1
+    gitconf push.default current
 
     gitconf color.ui true
-    gitconf color.diff-highlight.oldNormal    "red bold"
-    gitconf color.diff-highlight.oldHighlight "red bold 52"
-    gitconf color.diff-highlight.newNormal    "green bold"
-    gitconf color.diff-highlight.newHighlight "green bold 22"
-
     gitconf color.diff.meta       "yellow"
     gitconf color.diff.frag       "magenta bold"
     gitconf color.diff.commit     "yellow bold"
     gitconf color.diff.old        "red bold"
     gitconf color.diff.new        "green bold"
     gitconf color.diff.whitespace "red reverse"
+    gitconf color.diff-highlight.oldNormal    "red bold"
+    gitconf color.diff-highlight.oldHighlight "red bold 52"
+    gitconf color.diff-highlight.newNormal    "green bold"
+    gitconf color.diff-highlight.newHighlight "green bold 22"
+
+    gitconf diff-so-fancy.rulerWidth 47
+    gitconf diff-so-fancy.markEmptyLines false
+    gitconf diff-so-fancy.changeHunkIndicators false
+    gitconf diff-so-fancy.stripLeadingSymbols false
+
 }
 
 gitconf() {
